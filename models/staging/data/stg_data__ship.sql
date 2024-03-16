89bbf0d4c983caab6b5ff23d97455c754e77857d
Clara Wolff <clarawolff93@hotmail.com>
2024-03-16 17:43:44 +0000

    generated models and cleaned data

---
+++ models/staging/data/stg_data__ship.sql
@@ -0,0 +1,19 @@
+with 
+
+source as (
+
+    select * from {{ source('data', 'ship') }}
+
+),
+
+renamed as (
+
+    select
+        orders_id,
+        shipping_fee,
+        logcost,
+        ship_cost
+    from source
+)
+
+select * from renamed
