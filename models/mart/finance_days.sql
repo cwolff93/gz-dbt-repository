442056b7f50bcc40f6aa73a02ddddd69406ff9ec
Clara Wolff <clarawolff93@hotmail.com>
2024-03-16 20:43:04 +0000

    finance back to original attempt

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,11 +1,11 @@
-SELECT margin.date_date
+SELECT od_margin.date_date
 , COUNT(orders_id) AS nb_transactions
 , ROUND(SUM(revenue),2) AS revenue
 , ROUND(AVG(revenue),2) AS average_basket
-, ROUND(SUM(margin.margin),2) AS margin
+, ROUND(SUM(od_margin.margin),2) AS margin
 , ROUND(SUM(operational_margin),2) AS operational_margin
 FROM {{ref("int_orders_operational") }} AS operational
-JOIN {{ref("int_orders_margin") }} AS margin
+JOIN {{ref("int_orders_margin") }} AS od_margin
 USING (orders_id)
-GROUP BY margin.date_date
-ORDER BY margin.date_date DESC
+GROUP BY od_margin.date_date
+ORDER BY od_margin.date_date DESC
