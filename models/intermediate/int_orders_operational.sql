26c9a7ff9380f9414b94551cca94f4bda350cc06
Clara Wolff <clarawolff93@hotmail.com>
2024-03-16 20:40:15 +0000

    test

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,22 +1,6 @@
---SELECT orders_id
---, date_date
---, ROUND((margin + shipping_fee - logcost - ship_cost),2) AS operational_margin
---FROM {{ ref("int_orders_margin") }}
---JOIN {{ ref("stg_data__ship") }}
---USING(orders_id)
-
-SELECT
-  o.orders_id
-  ,o.date_date
-  ,ROUND(margin + s.shipping_fee - (s.logcost + CAST(s.ship_cost AS FLOAT64)), 2) AS operational_margin 
-  ,o.quantity
-  ,o.revenue
-  ,o.purchase_cost
-  ,o.margin
-  ,s.shipping_fee
-  ,s.logcost
-  ,s.ship_cost
-FROM {{ref("int_orders_margin")}} o
-LEFT JOIN {{ref("stg_data__ship")}} s 
-  USING(orders_id)
-ORDER BY orders_id desc
+SELECT orders_id
+, date_date
+, ROUND((margin + shipping_fee - logcost - ship_cost),2) AS operational_margin
+FROM {{ ref("int_orders_margin") }}
+JOIN {{ ref("stg_data__ship") }}
+USING(orders_id)
--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -2,10 +2,10 @@ SELECT margin.date_date
 , COUNT(orders_id) AS nb_transactions
 , ROUND(SUM(revenue),2) AS revenue
 , ROUND(AVG(revenue),2) AS average_basket
-, ROUND(SUM(margin),2) AS margin
+, ROUND(SUM(margin.margin),2) AS margin
 , ROUND(SUM(operational_margin),2) AS operational_margin
-FROM {{ ref("int_orders_margin") }} AS margin
-JOIN {{ ref("int_orders_operational") }} AS operational
-USING(orders_id)
+FROM {{ref("int_orders_operational") }} AS operational
+JOIN {{ref("int_orders_margin") }} AS margin
+USING (orders_id)
 GROUP BY margin.date_date
 ORDER BY margin.date_date DESC
