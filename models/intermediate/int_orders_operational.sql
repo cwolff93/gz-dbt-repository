21e3dc490113d05e2624a81401357099da9516f7
Clara Wolff <clarawolff93@hotmail.com>
2024-03-19 22:51:00 +0000

    finance days commit

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -8,17 +8,17 @@
 
 --Solution given by Le Wagon in order to build the model mart
 SELECT
-o.orders_id
-,o.date_date
-,ROUND(margin + s.shipping_fee - (s.logcost + CAST(s.ship_cost AS FLOAT64)), 2) AS operational_margin 
-,o.quantity
-,o.revenue
-,o.purchase_cost
-,o.margin
-,s.shipping_fee
-,s.logcost
-,s.ship_cost
+  o.orders_id
+  ,o.date_date
+  ,ROUND(o.margin + s.shipping_fee - (s.logcost + s.ship_cost),2) AS operational_margin 
+  ,o.quantity
+  ,o.revenue
+  ,o.purchase_cost
+  ,o.margin
+  ,s.shipping_fee
+  ,s.logcost
+  ,s.ship_cost
 FROM {{ref("int_orders_margin")}} o
 LEFT JOIN {{ref("stg_data__ship")}} s 
-USING(orders_id)
+  USING(orders_id)
 ORDER BY orders_id desc
