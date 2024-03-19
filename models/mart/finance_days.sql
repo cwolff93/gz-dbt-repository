7516ad39d36c6321d2cc2aaf9676492dc5be4301
Clara Wolff <clarawolff93@hotmail.com>
2024-03-19 22:31:44 +0000

    finance days commit

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,11 +1,17 @@
-SELECT od_margin.date_date
-, COUNT(orders_id) AS nb_transactions
-, ROUND(SUM(revenue),2) AS revenue
-, ROUND(AVG(revenue),2) AS average_basket
-, ROUND(SUM(od_margin.margin),2) AS margin
-, ROUND(SUM(operational_margin),2) AS operational_margin
-FROM {{ref("int_orders_operational") }} AS operational
-JOIN {{ref("int_orders_margin") }} AS od_margin
-USING (orders_id)
-GROUP BY od_margin.date_date
-ORDER BY od_margin.date_date DESC
+--{{config(materialized='')}}
+
+SELECT
+    date_date
+    ,COUNT(orders_id) AS nb_transactions
+    ,ROUND(SUM(revenue),0) AS revenue 
+    ,ROUND(AVG(revenue),1) AS average_basket
+    ,ROUND(SUM(margin),0) AS margin 
+    ,ROUND(SUM(operational_margin),0) AS operational_margin 
+    ,ROUND(SUM(purchase_cost),0) AS purchase_cost 
+    ,ROUND(SUM(shipping_fee),0) AS shipping_fee 
+    ,ROUND(SUM(logcost),0) AS logcost 
+    ,ROUND(SUM(ship_cost),0) AS ship_cost 
+    ,SUM(quantity) AS quantity 
+FROM {{ref("int_orders_operational")}}
+GROUP BY  date_date
+ORDER BY  date_date DESC
