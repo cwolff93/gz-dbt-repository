2fef254cc6146dd2987e28af635e04bd2fce61a9
clarawolff93@hotmail.com <clarawolff93@hotmail.com>
2024-03-22 00:49:17 +0000

    updates to documentation and use of dbt utils instead of union all

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,7 +1,14 @@
-SELECT * FROM {{ ref("stg_data__adwords")}}
-UNION ALL
-SELECT * FROM {{ ref("stg_data__bing")}}
-UNION ALL
-SELECT * FROM {{ ref("stg_data__criteo")}}
-UNION ALL
-SELECT * FROM {{ ref("stg_data__facebook")}}
+-- SELECT * FROM {{ ref("stg_data__adwords")}}
+-- UNION ALL
+-- SELECT * FROM {{ ref("stg_data__bing")}}
+-- UNION ALL
+-- SELECT * FROM {{ ref("stg_data__criteo")}}
+-- UNION ALL
+-- SELECT * FROM {{ ref("stg_data__facebook")}}
+
+{{ dbt_utils.union_relations(
+
+    relations=[ref('stg_data__adwords'), ref('stg_data__bing'), ref('stg_data__criteo'), ref('stg_data__facebook')],
+    source_column_name = None
+
+) }}
