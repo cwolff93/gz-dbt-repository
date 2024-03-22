0cad3098e1d14f8b0b3cac2c206733f059633e8f
clarawolff93@hotmail.com <clarawolff93@hotmail.com>
2024-03-22 00:18:13 +0000

    added packages and macros, added margin percent to intermediate sales margin model

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -6,7 +6,8 @@ SELECT
     revenue,
     quantity,
     quantity * purchase_price AS purchase_cost,
-    ROUND(revenue - (quantity * purchase_price),2) AS margin
+    ROUND(revenue - (quantity * purchase_price),2) AS margin,
+    {{ margin_percent ('revenue', 'quantity * purchase_price') }} AS margin_percent
 FROM {{ ref("stg_data__product") }}
 JOIN {{ ref("stg_data__sales") }} 
 USING (products_id)
